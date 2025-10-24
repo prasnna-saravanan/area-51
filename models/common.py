617b1e842f0ed3ad29d2ac6547c5195f1e6c920f
prasnna-saravanan <prasannasaravanan2002@gmail.com>
2025-10-24 20:57:48 +0530

    local

---
+++ models/common.py
@@ -0,0 +1,6 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class Filter(BaseModel):
+    filters: Optional[dict] = Field(default_factory=dict, description="Filters to apply to the data")
